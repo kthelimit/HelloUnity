dfb1a77ff1380ce9a28f39bbfb3e1269fa7a4529
K <yunsky2@naver.com>
2024-01-29 17:47:42 +0900

    29일자
    과제로 나왔던 표창 날리기를 함께 제작해보고
    고양이 탈출게임(고양이를 조작해 화살표를 피하는 게임)을 제작했다.
    고양이를 키보드로 조작하고, UI에 이미지를 띄워 고양이의 체력을 표시하는 법, 그리고 화살을 생성하는 법을 학습했다.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -39,6 +39,12 @@ public class CarController : MonoBehaviour
             speed = length / divide;
             //Debug.LogFormat("<color=yellow>speed : {0}</color>", speed);
             //화면에서 손을 뗀 지점의 x - 터치한 지점의 x
+            //사운드 실행
+
+            //게임오브젝트에 붙어있는 컴포넌트 가져오기
+            //this. gameObject.GetComponent<AudioSource>()
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
         }
         //0.1유닛씩 매 프레임마다 이동한다.
         this.gameObject.transform.Translate(new Vector3(speed, 0, 0));
